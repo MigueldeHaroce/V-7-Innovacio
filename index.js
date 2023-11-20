4243c691a5824b50dbbdce8c8d6f02d4a449a5f5
MigueldeHaroce <migueldeharoce@gmail.com>
2023-11-20 20:44:50 +0100

    Trying with blur

--- index.js
+++ index.js
@@ -451,56 +451,71 @@ window.initMap = initMap;
 
 // ==== Items ==== 
 
-
 function expandItem(item) {
 	const items = document.querySelectorAll('.item');
 	items.forEach((el) => {
-	  el.classList.remove('expanded');
+		el.classList.remove('expanded');
+		el.classList.remove('blur');
 	});
-  
-	item.classList.add('expanded');
+
+	if (item.classList.contains('expanded')) {
+		item.classList.remove('expanded');
+		item.classList.remove('blur');
+	} else {
+		item.classList.add('expanded');
+		item.classList.add('blur');
+	}
 }
 
 $(function () {
-  var galleryThumbs = new Swiper(".gallery-thumbs", {
-    centeredSlides: true,
-    centeredSlidesBounds: true, 
-    direction: "horizontal",
-    spaceBetween: 10,
-    slidesPerView: 3,
-    freeMode: false,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    watchOverflow: true,
-    breakpoints: {
-      480: {
-        direction: "vertical",
-        slidesPerView: 3
-      }
-    }
-  });
-  var galleryTop = new Swiper(".gallery-top", {
-    direction: "horizontal",
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    a11y: {
-      prevSlideMessage: "Previous slide",
-      nextSlideMessage: "Next slide",
-    },
-    keyboard: {
-      enabled: true,
-    },
-    thumbs: {
-      swiper: galleryThumbs
-    }
-  });
-  galleryTop.on("slideChangeTransitionStart", function () {
-    galleryThumbs.slideTo(galleryTop.activeIndex);
-  });
-  galleryThumbs.on("transitionStart", function () {
-    galleryTop.slideTo(galleryThumbs.activeIndex);
-  });
+	var galleryThumbs = new Swiper(".gallery-thumbs", {
+		centeredSlides: true,
+		centeredSlidesBounds: true, 
+		direction: "horizontal",
+		spaceBetween: 10,
+		slidesPerView: 3,
+		freeMode: false,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+		watchOverflow: true,
+		breakpoints: {
+			480: {
+				direction: "vertical",
+				slidesPerView: 3
+			}
+		}
+	});
+	var galleryTop = new Swiper(".gallery-top", {
+		direction: "horizontal",
+		spaceBetween: 10,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev"
+		},
+		a11y: {
+			prevSlideMessage: "Previous slide",
+			nextSlideMessage: "Next slide",
+		},
+		keyboard: {
+			enabled: true,
+		},
+		thumbs: {
+			swiper: galleryThumbs
+		}
+	});
+	galleryTop.on("slideChangeTransitionStart", function () {
+		galleryThumbs.slideTo(galleryTop.activeIndex);
+	});
+	galleryThumbs.on("transitionStart", function () {
+		galleryTop.slideTo(galleryThumbs.activeIndex);
+	});
+
+	const descriptionBtn = document.getElementById('descriptionBtn');
+	descriptionBtn.addEventListener('click', function() {
+		const activeSlide = document.querySelector('.swiper-slide-active');
+		if (activeSlide) {
+			expandItem(activeSlide);
+			console.log(activeSlide);
+		}
+	});
 });
